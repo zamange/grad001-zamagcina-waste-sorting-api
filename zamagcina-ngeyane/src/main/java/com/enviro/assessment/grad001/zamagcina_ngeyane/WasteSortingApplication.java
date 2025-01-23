dbba860cd03b722b14fe1355d201413430bd2e19
Zamagcina Ngeyane <zamagcinangeyane6@gmail.com>
2025-01-23 18:21:13 +0200

    fixed project structure

---
+++ zamagcina-ngeyane/src/main/java/com/enviro/assessment/grad001/zamagcina_ngeyane/WasteSortingApplication.java
@@ -0,0 +1,13 @@
+package com.enviro.assessment.grad001.zamagcina_ngeyane;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WasteSortingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WasteSortingApplication.class, args);
+	}
+
+}
---
+++ zamagcina-ngeyane/src/test/java/com/enviro/assessment/grad001/zamagcina_ngeyane/ZamagcinaNgeyaneApplicationTests.java
@@ -0,0 +1,13 @@
+package com.enviro.assessment.grad001.zamagcina_ngeyane;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ZamagcinaNgeyaneApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
