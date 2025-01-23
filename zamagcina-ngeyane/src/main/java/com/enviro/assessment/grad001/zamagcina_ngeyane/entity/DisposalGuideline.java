f96ab6b3c6bad553220ffb5b208d9ab87d2c6917
Zamagcina Ngeyane <zamagcinangeyane6@gmail.com>
2025-01-23 18:27:58 +0200

    disposable guideline class

--- zamagcina-ngeyane/src/main/java/com/enviro/assessment/grad001/zamagcina_ngeyane/entity/DisposalGuideline.java
+++ zamagcina-ngeyane/src/main/java/com/enviro/assessment/grad001/zamagcina_ngeyane/entity/DisposalGuideline.java
@@ -1,4 +1,19 @@
 package com.enviro.assessment.grad001.zamagcina_ngeyane.entity;
 
+import jakarta.persistence.*;
+import lombok.Data;
+
+@Entity
+@Data
 public class DisposalGuideline {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @ManyToOne
+    @JoinColumn(name = "waste_category_id", nullable = false)
+    private WasteCategory wasteCategory;
+
+    @Column(nullable = false, length = 1000)
+    private String guideline;
 }
