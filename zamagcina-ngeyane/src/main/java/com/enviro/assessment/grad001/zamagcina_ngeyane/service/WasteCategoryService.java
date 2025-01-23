1040e59867d37351a9d92e3a38030f8d2c800062
Zamagcina Ngeyane <zamagcinangeyane6@gmail.com>
2025-01-23 18:36:34 +0200

    waste category service class

---
+++ zamagcina-ngeyane/src/main/java/com/enviro/assessment/grad001/zamagcina_ngeyane/service/WasteCategoryService.java
@@ -0,0 +1,44 @@
+package com.enviro.assessment.grad001.zamagcina_ngeyane.service;
+
+import com.enviro.assessment.grad001.zamagcina_ngeyane.entity.WasteCategory;
+import com.enviro.assessment.grad001.zamagcina_ngeyane.repository.WasteCategoryRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
+
+@Service
+public class WasteCategoryService {
+
+    private final WasteCategoryRepository wasteCategoryRepository;
+
+    @Autowired
+    public WasteCategoryService(WasteCategoryRepository wasteCategoryRepository) {
+        this.wasteCategoryRepository = wasteCategoryRepository;
+    }
+
+    public List<WasteCategory> getAllCategories() {
+        return wasteCategoryRepository.findAll();
+    }
+
+    public WasteCategory getCategoryById(Long id) {
+        return wasteCategoryRepository.findById(id).orElseThrow(() ->
+                new RuntimeException("Waste category not found with id: " + id));
+    }
+
+    public WasteCategory addCategory(WasteCategory wasteCategory) {
+        return wasteCategoryRepository.save(wasteCategory);
+    }
+
+    public WasteCategory updateCategory(Long id, WasteCategory updatedCategory) {
+        WasteCategory existingCategory = getCategoryById(id);
+        existingCategory.setName(updatedCategory.getName());
+        existingCategory.setDescription(updatedCategory.getDescription());
+        return wasteCategoryRepository.save(existingCategory);
+    }
+
+    public void deleteCategory(Long id) {
+        wasteCategoryRepository.deleteById(id);
+    }
+}
