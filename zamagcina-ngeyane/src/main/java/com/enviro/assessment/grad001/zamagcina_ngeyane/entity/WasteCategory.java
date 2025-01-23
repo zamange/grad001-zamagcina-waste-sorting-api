90b3f8436461b5ec53678b57d711e44dd62a964d
Zamagcina Ngeyane <zamagcinangeyane6@gmail.com>
2025-01-23 18:28:40 +0200

    waste category class class

--- zamagcina-ngeyane/src/main/java/com/enviro/assessment/grad001/zamagcina_ngeyane/entity/WasteCategory.java
+++ zamagcina-ngeyane/src/main/java/com/enviro/assessment/grad001/zamagcina_ngeyane/entity/WasteCategory.java
@@ -1,4 +1,18 @@
 package com.enviro.assessment.grad001.zamagcina_ngeyane.entity;
 
+import jakarta.persistence.*;
+import lombok.Data;
+
+@Entity
+@Data
 public class WasteCategory {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false, unique = true)
+    private String name;
+
+    @Column(length = 500)
+    private String description;
 }
